fecc523c365a74cb4b9849d2a866ced86f6f76f9
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-02-04 19:42:52 +0200

    create global exception handler

---
+++ src/main/java/me/tbandawa/notesapi/exception/RestExceptionHandler.java
@@ -0,0 +1,55 @@
+package me.tbandawa.notesapi.exception;
+
+import java.time.ZoneOffset;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+	
+    @ExceptionHandler({NoteNotFoundException.class})
+    public ResponseEntity<ErrorResponse> noteNotFound(NoteNotFoundException ex, WebRequest request){
+        ErrorResponse apiResponse =new ErrorResponse.ErrorResponseBuilder()
+                                .withDetail("Not able to find customer record")
+                                .withMessage("Not a valid user id.Please provide a valid user id or contact system admin.")
+                                .withErrorCode("404")
+                                .withStatus(HttpStatus.NOT_FOUND)
+                                .withTimeStamp(LocalDateTime.now(ZoneOffset.UTC))
+                                .build();
+        return new ResponseEntity<ErrorResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+    
+    @ExceptionHandler(NoteServiceException.class)
+    protected ResponseEntity<Object> handleCustomAPIException(NoteServiceException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    	ErrorResponse response =new ErrorResponse.ErrorResponseBuilder()
+                .withStatus(status)
+                .withDetail("custom exception")
+                .withMessage(ex.getLocalizedMessage())
+                .withErrorCode("503")
+                .withStatus(status.SERVICE_UNAVAILABLE)
+                .withTimeStamp(LocalDateTime.now(ZoneOffset.UTC))
+                .build();
+        return new ResponseEntity<>(response, response.getHttpStatus());
+    }
+    
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Object> handleCustomAPIException(Exception ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    	ErrorResponse response =new ErrorResponse.ErrorResponseBuilder()
+                .withStatus(status)
+                .withDetail("Something went wrong")
+                .withMessage(ex.getLocalizedMessage())
+                .withErrorCode("502")
+                .withStatus(status.BAD_GATEWAY)
+                .withTimeStamp(LocalDateTime.now(ZoneOffset.UTC))
+                .build();
+        return new ResponseEntity<>(response, response.getHttpStatus());
+    }
+
+}
